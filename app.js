ee2753ea348b513ee468c161bf1998a60d364c2a
Sam Puddister <sampuddister@gmail.com>
2024-02-09 14:52:53 -0500

    Added models and controllers for all model types.

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const logger = require("morgan");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 const app = express();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB =
+  "mongodb+srv://spuddister:database321@cluster0.qhhcxbp.mongodb.net/local_library?retryWrites=true&w=majority";
+
+DbConnect().catch((err) => console.log(err));
+async function DbConnect() {
+  await mongoose.connect(mongoDB);
+}
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -21,6 +33,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
